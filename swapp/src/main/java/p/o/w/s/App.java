bd8ccea3579711df22db57564227b28f9ae0b253
jdewaen <jorik.de.waen@gmail.com>
2015-02-12 17:25:53 +0100

    hello worldgit

---
+++ swapp/src/main/java/p/o/w/s/App.java
@@ -0,0 +1,13 @@
+package p.o.w.s;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
