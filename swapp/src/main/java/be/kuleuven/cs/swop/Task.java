74f8c3297f9a479fb489e327e94a3a22258eaa66
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-02-18 18:48:15 +0100

    more on task

--- swapp/src/main/java/be/kuleuven/cs/swop/Task.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/Task.java
@@ -1,23 +1,28 @@
 package be.kuleuven.cs.swop;
 
+import java.util.Set;
+
 public class Task {
 
 	private String description;
 	private Project project;
 	private Duration estimatedDuration;
 	private TaskStatus status;
+	private Set<Task> dependencies;
+	private Task original;
 
 	public String getDescription() {
 		return description;
-
 	}
 
-	public static boolean canHaveAsDescription(String description) {
+	protected static boolean canHaveAsDescription(String description) {
 		return description != null;
 	}
 
-	public void setDescription(String description) {
-		if (!canHaveAsDescription(description)){ throw new IllegalArgumentException(ERROR_ILLEGAL_DESCRIPTION);}
+	private void setDescription(String description) {
+		if (!canHaveAsDescription(description)) {
+			throw new IllegalArgumentException(ERROR_ILLEGAL_DESCRIPTION);
+		}
 		this.description = description;
 	}
 
@@ -25,12 +30,14 @@ public class Task {
 		return project;
 	}
 
-	public static boolean canHaveAsProject(Project project) {
+	protected static boolean canHaveAsProject(Project project) {
 		return project != null;
 	}
 
 	public void setProject(Project project) {
-		if (!canHaveAsProject(project)){throw new IllegalArgumentException(ERROR_ILLEGAL_PROJECT);}
+		if (!canHaveAsProject(project)) {
+			throw new IllegalArgumentException(ERROR_ILLEGAL_PROJECT);
+		}
 		this.project = project;
 	}
 
@@ -38,7 +45,7 @@ public class Task {
 		return estimatedDuration;
 	}
 
-	public static boolean canHaveAsEstimatedDuration(Duration estimatedDuration) {
+	protected static boolean canHaveAsEstimatedDuration(Duration estimatedDuration) {
 		return estimatedDuration != null;
 	}
 
@@ -53,7 +60,7 @@ public class Task {
 		return status;
 	}
 
-	private static boolean canHaveAsStatus(TaskStatus status) {
+	protected static boolean canHaveAsStatus(TaskStatus status) {
 		return status != null;
 	}
 
@@ -64,8 +71,53 @@ public class Task {
 		this.status = status;
 	}
 
+	public Task getOriginal() {
+		return original;
+	}
+
+	protected static boolean canHaveAsOriginal(Task original) {
+		return true;
+	}
+
+	public void setOriginal(Task original) {
+		if (!canHaveAsOriginal(original)) {
+			throw new IllegalArgumentException(ERROR_ILLEGAL_ORIGINAL);
+		}
+		this.original = original;
+	}
+
+	public Set<Task> getDependencySet() {
+		return dependencies;
+	}
+
+	private static boolean canHaveAsDependencySet(Set<Task> dependencyList) {
+		return dependencyList != null;
+	}
+
+	public void setDependencySet(Set<Task> dependencySet) {
+		if (!canHaveAsDependencySet(dependencySet)) {
+			throw new IllegalArgumentException(ERROR_ILLEGAL_DEPENDENCY_SET);
+		}
+		this.dependencies = dependencySet;
+	}
+
+	public static boolean canHaveAsDependency(Task dependency) {
+		return dependency != null;
+	}
+
+	public void addDependency(Task dependency) {
+		if (!canHaveAsDependency(dependency)) {
+			throw new IllegalArgumentException(ERROR_ILLEGAL_DEPENDENCY);
+		}
+		this.dependencies.add(dependency);
+	}
+
 	private static final String ERROR_ILLEGAL_DESCRIPTION = "Illegal project for task.";
 	private static final String ERROR_ILLEGAL_PROJECT = "Illegal project for task.";
 	private static final String ERROR_ILLEGAL_DURATION = "Illegal estimated duration for task.";
 	private static final String ERROR_ILLEGAL_STATUS = "Illegal status for task.";
+	private static final String ERROR_ILLEGAL_ORIGINAL = "Illegal original for task.";
+	private static final String ERROR_ILLEGAL_DEPENDENCY = "Illegal dependency set for task.";
+	private static final String ERROR_ILLEGAL_DEPENDENCY_SET = "Illegal dependency for task.";
+
 }
