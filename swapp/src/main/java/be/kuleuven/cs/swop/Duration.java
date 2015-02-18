d0ea01192e80cf394bf6239d4d4fadc56b08b2dc
jdewaen <jorik.de.waen@gmail.com>
2015-02-18 18:21:05 +0100

    implemented duration

--- swapp/src/main/java/be/kuleuven/cs/swop/Duration.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/Duration.java
@@ -1,5 +1,20 @@
 package be.kuleuven.cs.swop;
 
 public class Duration {
+	private int minutes;
 
+	public Duration(int minutes){
+		setMinutes(minutes);
+	}
+
+	private void setMinutes(int minutes){
+		this.minutes = minutes;
+	}
+	public int getMinutes(){
+		return minutes;
+	}
+
+	public static boolean canHaveAsMinutes(int input){
+		return (input > 0);
+	}
 }
