d79c3a84e7614c7f632d7814cd64e5e3e19d9444
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-03-04 17:41:45 +0100

    Facade now makes its ProjectManager.

--- swapp/src/main/java/be/kuleuven/cs/swop/FacadeController.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/FacadeController.java
@@ -2,18 +2,21 @@ package be.kuleuven.cs.swop;
 
 import java.util.Set;
 
+import be.kuleuven.cs.swop.domain.ProjectManager;
 import be.kuleuven.cs.swop.domain.project.Project;
 
 
 public class FacadeController {
+	
+	ProjectManager projectManager;
     
     public FacadeController(){
-    	
+    	projectManager = new ProjectManager();
     }
     
     public Set<Project> getProjects() {
-		throw new UnsupportedOperationException("Can't get the projects yet.");
-	}
+    	return projectManager.getProjects();
+    }
     
     public void initialiseWith(String initFile){
         // TODO initialise with file.
