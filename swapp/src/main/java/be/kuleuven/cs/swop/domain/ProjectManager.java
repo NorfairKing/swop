79ae4c7f0e89af6803e4e168f9786636bdf1d2da
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-03-04 17:25:45 +0100

    Added projectmanager.

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/ProjectManager.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/ProjectManager.java
@@ -1,6 +1,8 @@
 package be.kuleuven.cs.swop.domain;
 
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,21 +11,40 @@ import be.kuleuven.cs.swop.domain.project.Project;
 
 public class ProjectManager {
 
-    private Set<Project> projects = new HashSet<Project>();
-
-    public ProjectManager() {}
+    private final Set<Project> projects = new HashSet<Project>();
+    private Calendar currentTime;
+    
+    public ProjectManager() {
+    	currentTime = Calendar.getInstance();
+    	currentTime.setTimeInMillis(0);
+    }
 
     public Set<Project> getProjects() {
         return projects;
+    }
+    
+    public Project getProject(String title) {
+    	for (Project project: projects) {
+    		if (project.getTitle().equals(title)) {
+    			return project;
+    		}
+    	}
+    	return null;
     }
 
     protected boolean canHaveAsProject(Project project) {
         return project != null;
     }
     
-    public void addProject(Project project){
-        if (!canHaveAsProject(project) )throw new IllegalArgumentException(ERROR_ILLEGAL_PROJECT);
+    private void addProject(Project project){
+        if (!canHaveAsProject(project) ) throw new IllegalArgumentException(ERROR_ILLEGAL_PROJECT);
         projects.add(project);
+    }
+    
+    public void createProject(String title, String description, Date dueTime) {
+    	Date creationTime = null; //get current time from Syd
+    	Project project = new Project(title, description, creationTime, dueTime);
+    	addProject(project);
     }
 
     private static final String ERROR_ILLEGAL_PROJECT = "Invalid project for project manager";
