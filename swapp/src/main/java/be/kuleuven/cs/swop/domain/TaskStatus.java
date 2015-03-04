f95048cdc4af6a881f24319a214c511bd698eec4
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-03-04 12:36:21 +0100

    started on task status

---
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/TaskStatus.java
@@ -0,0 +1,26 @@
+package be.kuleuven.cs.swop.domain;
+
+
+public abstract class TaskStatus {
+
+    private Task task;
+
+    public TaskStatus(Task task) {
+        setTask(task);
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    protected static boolean canHaveAsTask(Task task) {
+        return task != null;
+    }
+
+    private void setTask(Task task) {
+        if (!canHaveAsTask(task)) throw new IllegalArgumentException(ERROR_ILLEGAL_TASK);
+        this.task = task;
+    }
+
+    private static final String ERROR_ILLEGAL_TASK = "Invalid task for task status";
+}
