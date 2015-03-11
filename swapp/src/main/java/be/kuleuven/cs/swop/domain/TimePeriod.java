bb48103a7c1f90e960d426c9b40dc8737a8f9c7f
Michiel Derhaeg <derhaeg_michiel@hotmail.com>
2015-03-11 15:54:36 +0100

    added documentation for TimePeriod

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/TimePeriod.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/TimePeriod.java
@@ -9,15 +9,39 @@ public class TimePeriod {
     private Date startTime;
     private Date stopTime;
 
+    /**
+     * Full Constructor
+     *
+     * @param start The Date containing the start of this peroid.
+     *
+     * @param stop The Date containing the end of this peroid.
+     *
+     */
     public TimePeriod(Date start, Date stop) {
         setStartTime(start);
         setStopTime(stop);
     }
 
+    /**
+     * Retries the time on which this period starts.
+     *
+     * @return The Date containing the start of this period.
+     *
+     */
     public Date getStartTime() {
         return (Date) startTime.clone();
     }
 
+    /**
+     * Checks whether or not the given time is a valid beginning for this period,
+     * it's valid when the Date isn't null.
+     *
+     * @param startTime The Date containing the time to be checked if it is a valid
+     * beginning for this perdiod.
+     *
+     * @return Returns true if the given time is a valid beginning for the period.
+     *
+     */
     protected boolean canHaveAsStartTime(Date startTime) {
         return startTime != null;
     }
@@ -27,21 +51,35 @@ public class TimePeriod {
         this.startTime = startTime;
     }
 
+    /**
+     * Retries the time on which this period ends.
+     *
+     * @return The Date containing the end of this period.
+     *
+     */
     public Date getStopTime() {
         return (Date) stopTime.clone();
     }
 
+    /**
+     * Checks whether or not the given time is a valid ending for this period,
+     * it's valid when the Date isn't null.
+     *
+     * @param stopTime The Date containing the time to be checked if it is a valid
+     * ending for this perdiod.
+     *
+     * @return Returns true if the given time is a valid beginning for the period.
+     *
+     */
     protected boolean canHaveAsStopTime(Date stopTime) {
         return stopTime != null && startTime != null && startTime.before(stopTime);
     }
 
     /**
      * Has to be used AFTER setStartTime().
-     * 
-     * @param stopTime
      */
     private void setStopTime(Date stopTime) {
-        if (!canHaveAsStartTime(startTime)) throw new IllegalArgumentException(ERROR_ILLEGAL_STOP_TIME);
+        if (!canHaveAsStopTime(stopTime)) throw new IllegalArgumentException(ERROR_ILLEGAL_STOP_TIME);
         this.stopTime = stopTime;
     }
 
