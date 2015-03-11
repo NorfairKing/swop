88a8522d63d34e32acba9156fc39bdd8392eead7
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-03-11 12:23:18 +0100

    canhaveasdependency tested

--- swapp/src/test/java/be/kuleuven/cs/swop/domain/task/TaskTest.java
+++ swapp/src/test/java/be/kuleuven/cs/swop/domain/task/TaskTest.java
@@ -31,5 +31,49 @@ public class TaskTest {
     public void test() {
         // TODO test something
     }
+    
+    @Test
+    public void canHaveAsDependencyTest() {
+        Task t1 = new Task("task1",50,0);
+        Task t2 = new Task("task2",60,0);
+        Task t3 = new Task("task3",60,0);
+        Task t4 = new Task("task4",60,0);
+        Task t5 = new Task("task5",60,0);
+        
+        // null can never be a depencency
+        assertFalse(t1.canHaveAsDependency(null));
+        
+        // A task cannot depend on itself
+        assertFalse(t1.canHaveAsDependency(t1));
+        
+        // Some valid cases
+        assertTrue(t1.canHaveAsDependency(t2));
+        assertTrue(t1.canHaveAsDependency(t4));
+        
+        t1.addDependency(t2);
+        t1.addDependency(t4);
+        
+        assertTrue(t2.canHaveAsDependency(t3));
+        assertTrue(t2.canHaveAsDependency(t3));
+        
+        t2.addDependency(t3);
+        t2.addDependency(t4);
+        
+        assertTrue(t3.canHaveAsDependency(t5));
+        t3.addDependency(t5);
+        t4.addDependency(t5);
+        
+        // Tests for dependency loops
+        assertFalse(t2.canHaveAsDependency(t1));
+        
+        assertFalse(t4.canHaveAsDependency(t1));
+        assertFalse(t4.canHaveAsDependency(t2));
+        
+        assertFalse(t5.canHaveAsDependency(t1));
+        assertFalse(t5.canHaveAsDependency(t2));
+        assertFalse(t5.canHaveAsDependency(t3));
+        assertFalse(t5.canHaveAsDependency(t4));
+        
+    }
 
 }
