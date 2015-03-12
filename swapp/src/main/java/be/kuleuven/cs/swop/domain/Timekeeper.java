0628eb3f99233b0021a3441d6a93878f3b9b2888
jdewaen <jorik.de.waen@gmail.com>
2015-03-12 16:23:58 +0100

    Implemented workingMinutesBetween

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/Timekeeper.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/Timekeeper.java
@@ -1,6 +1,10 @@
 package be.kuleuven.cs.swop.domain;
 
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -8,6 +12,9 @@ import java.util.Date;
 public final class Timekeeper {
 
     private static Calendar currentTime = Calendar.getInstance();
+    private static int workDayStart = 8;
+    private static int workDayEnd = 16;
+    private static DayOfWeek[] workDays = new DayOfWeek[]{DayOfWeek.MONDAY,DayOfWeek.TUESDAY,DayOfWeek.WEDNESDAY,DayOfWeek.THURSDAY,DayOfWeek.FRIDAY};
 
     /**
      * Return the current system time.
@@ -44,6 +51,73 @@ public final class Timekeeper {
     public static void setTime(Date time) {
         if (!canHaveAsTime(time)) { throw new IllegalArgumentException("Invalid time for the system."); }
         currentTime.setTime(time);
+    }
+
+    private static LocalDateTime startOfDay(LocalDateTime input){
+        return input.withSecond(0).withMinute(0).withHour(0);
+    }
+    private static LocalDateTime endOfDay(LocalDateTime input){
+        return startOfDay(input).plusDays(1);
+    }
+
+    
+    // time1 and time2 need to be on the same day!!!
+    private static int timeInSingleDayBetween(LocalDateTime time1, LocalDateTime time2){
+        LocalDateTime startOfWorkDay = time1.withHour(workDayStart).withMinute(0).withSecond(0);
+        LocalDateTime endOfWorkDay = time1.withHour(workDayEnd).withMinute(0).withSecond(0);
+        LocalDateTime countStart;
+        LocalDateTime countStop;
+        if(! isWorkDay(time1)){
+            return 0;
+        }
+        if(startOfWorkDay.isBefore(time1)){ 
+            countStart = time1;
+        }else{
+            countStart = startOfWorkDay;
+        }
+
+        if(endOfWorkDay.isBefore(time2)){
+            countStop = endOfWorkDay;
+        }else{
+            countStop = time2;
+        }
+
+        System.out.println(countStart.toString());
+        System.out.println(countStop.toString());
+
+        int difference = (int) ChronoUnit.MINUTES.between(countStart,countStop);
+        if(difference > 0){
+            return difference;
+        }else{
+            return 0;
+        }
+
+    }
+    private static boolean isWorkDay(LocalDateTime time){
+        DayOfWeek day = time.getDayOfWeek();
+        for(DayOfWeek d: workDays){
+            if(d == day){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int workingMinutesBetween(Date time1, Date time2){
+        LocalDateTime start = LocalDateTime.ofEpochSecond(time1.getTime() / 1000, 0, ZoneOffset.UTC);
+        LocalDateTime stop = LocalDateTime.ofEpochSecond(time2.getTime() / 1000, 0, ZoneOffset.UTC);
+        LocalDateTime endOfDay = endOfDay(start);
+        int workMinutes = 0;
+
+        while(endOfDay.isBefore(stop)){
+            workMinutes += timeInSingleDayBetween(start, endOfDay);
+
+            start = endOfDay;
+            endOfDay = endOfDay.plusDays(1);
+        }
+
+        workMinutes += timeInSingleDayBetween(start, stop);
+        return workMinutes;
     }
 
 }
