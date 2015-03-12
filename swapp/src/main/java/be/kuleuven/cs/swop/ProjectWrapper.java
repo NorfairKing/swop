04fb19cc5187110168c7820fa7c1489ea34694ce
Michiel Derhaeg <derhaeg_michiel@hotmail.com>
2015-03-12 15:57:21 +0100

    added docs for the wrappers

--- swapp/src/main/java/be/kuleuven/cs/swop/ProjectWrapper.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/ProjectWrapper.java
@@ -13,14 +13,35 @@ public class ProjectWrapper {
 
     private Project project;
 
+    /**
+     * Full contstructor
+     *
+     * @param project The Project this wrapper has to contain.
+     *
+     */
     public ProjectWrapper(Project project) {
         setProject(project);
     }
 
+    /**
+     * Retrieves the the Project contained by this wrapper.
+     *
+     * @return Returns the Project contained by this ProjectWrapper.
+     *
+     */
     Project getProject() {
         return project;
     }
 
+    /**
+     * Checks whether or not this wrapper can wrap around the given Project,
+     * the ProjectWrapper can't have the given Project if it's null.
+     *
+     * @param project The Project to be checked for validity.
+     *
+     * @return Returns true if the given Project is valid.
+     *
+     */
     protected boolean canHaveAsProject(Project project) {
         return project != null;
     }
@@ -30,37 +51,93 @@ public class ProjectWrapper {
         this.project = project;
     }
 
+    /**
+     * Retrieve the title of the Project contained by this wrapper.
+     *
+     * @return Returns a String containing the Project's title.
+     *
+     */
     public String getTitle() {
         return getProject().getTitle();
     }
 
+    /**
+     * Retrieve the description of the Project contained by this wrapper.
+     *
+     * @return Returns a String containing the Project's description.
+     *
+     */
     public String getDescription() {
         return getProject().getDescription();
     }
 
+    /**
+     * Checks whether or not the contained Project is ongoing.
+     *
+     * @return Returns true if the containing Project is ongoing.
+     *
+     */
     public boolean isOngoing() {
         return getProject().isOngoing();
     }
 
+    /**
+     * Checks whether or not the contained Project is finished.
+     *
+     * @return Returns true if the containing Project is finished.
+     *
+     */
     public boolean isFinished() {
         return getProject().isFinished();
     }
 
+    /**
+     * Retrieve the creation time of the Project contained by this wrapper.
+     *
+     * @return Returns the Date containing the Project's creation time.
+     *
+     */
     public Date getCreationTime() {
         return getProject().getCreationTime();
     }
 
+    /**
+     * Retrieve the due time of the Project contained by this wrapper.
+     *
+     * @return Returns the Date containing the Project's due time.
+     *
+     */
     public Date getDueTime() {
         return getProject().getDueTime();
     }
-    
+
+    /**
+     * Checks whether or not the contained Project is on time.
+     *
+     * @return Returns true if the containing Project is on time.
+     *
+     */
     public boolean isOnTime(){
         return getProject().isOnTime();
     }
+
+    /**
+     * Checks whether or not the contained Project isn't on time.
+     *
+     * @return Returns true if the containing Project isn't on time.
+     *
+     */
     public boolean isOverTime(){
         return getProject().isOverTime();
     }
 
+    /**
+     * Retrieve the Tasks of the Project contained by this wrapper.
+     *
+     * @return Returns a Set containing TaskWrappers of the Tasks from the Project
+     * contained by this wrapper.
+     *
+     */
     public Set<TaskWrapper> getTasks() {
         Set<TaskWrapper> result = new HashSet<TaskWrapper>();
         for (Task realTask : project.getTasks()) {
--- swapp/src/main/java/be/kuleuven/cs/swop/TaskWrapper.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/TaskWrapper.java
@@ -11,14 +11,35 @@ public class TaskWrapper {
 
     private Task task;
 
+    /**
+     * Full contstructor
+     *
+     * @param task The Task this wrapper has to contain.
+     *
+     */
     public TaskWrapper(Task task) {
         setTask(task);
     }
 
+    /**
+     * Retrieves the the Task contained by this wrapper.
+     *
+     * @return Returns the Task contained by this TaskWrapper.
+     *
+     */
     Task getTask() {
         return task;
     }
 
+    /**
+     * Checks whether or not this wrapper can wrap around the given Task,
+     * the TaskWrapper can't have the given Task if it's null.
+     *
+     * @param Task The Task to be checked for validity.
+     *
+     * @return Returns true if the given Task is valid.
+     *
+     */
     protected boolean canHaveAsTask(Task task) {
         return task != null;
     }
@@ -28,18 +49,43 @@ public class TaskWrapper {
         this.task = task;
     }
 
+    /**
+     * Retrieve the description of the Task contained by this wrapper.
+     *
+     * @return Returns a String containing the Task's description.
+     *
+     */
     public String getDescription() {
         return getTask().getDescription();
     }
 
+    /**
+     * Retrieve the contained Task's estimated duration in minutes.
+     *
+     * @return Returns a double which represents the Task's estimated duration in minutes.
+     *
+     */
     public double getEstimatedDuration() {
         return getTask().getEstimatedDuration();
     }
 
+    /**
+     * Retrieve the contained Task's acceptable deviation in minutes.
+     *
+     * @return Returns a double which represents the Task's acceptable deviation in minutes.
+     *
+     */
     public double getAcceptableDeviation() {
         return getTask().getAcceptableDeviation();
     }
 
+    /**
+     * Retrieves the containing Task's alternative Task for when the Task has failed.
+     *
+     * @return Returns a TaskWrapper containing the alternative Task.
+     *         Returns null when the containing Task doesn't have an alternative.
+     *
+     */
     public TaskWrapper getAlternative() {
         Task alt = getTask().getAlternative();
         if (alt == null) {
@@ -49,34 +95,81 @@ public class TaskWrapper {
         }
     }
 
+    /**
+     * Retrieves the time period when the containing Task gets performed during.
+     *
+     * @return Returns the TimePeriod containing for when this Task is performed during.
+     */
     public TimePeriod getPerformedDuring() {
         return getTask().getPerformedDuring();
     }
 
+    /**
+     * Retrieves the containing Task's dependencies.
+     *
+     * @return Returns a Set containing the Tasks which are dependencies to this Task.
+     *
+     */
     public Set<Task> getDependencySet() {
         return getTask().getDependencySet();
     }
 
+    /**
+     * Checks whether or not the containing Task is finished.
+     *
+     * @return Returns true if the containing Task is finished.
+     *
+     */
     public boolean isFinished() {
         return getTask().isFinished();
     }
 
+    /**
+     * Checks whether or not the containing Task is failed.
+     *
+     * @return Returns true if the containing Task is failed.
+     *
+     */
     public boolean isFailed() {
         return getTask().isFailed();
     }
-    
+
+    /**
+     * Checks whether or not the containing Task can finish.
+     *
+     * @return Returns true if the containing Task can finish.
+     *
+     */
     public boolean canFinish(){
         return getTask().canFinish();
     }
 
+    /**
+     * Checks whether or not the containing Task was finished early.
+     *
+     * @return Returns true if the containing Task was finished early.
+     *
+     */
     public boolean wasFinishedEarly() {
         return task.wasFinishedEarly();
     }
 
+    /**
+     * Checks whether or not the containing Task was finished on time.
+     *
+     * @return Returns true if the containing Task was finished on time.
+     *
+     */
     public boolean wasFinishedOnTime() {
         return getTask().wasFinishedOnTime();
     }
 
+    /**
+     * Checks whether or not the containing Task was finished late.
+     *
+     * @return Returns true if the containing Task was finished late.
+     *
+     */
     public boolean wasFinishedLate() {
         return getTask().wasFinishedLate();
     }
