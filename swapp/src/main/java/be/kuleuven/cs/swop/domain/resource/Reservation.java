10ff9efdb49bcad840d8aac2d84714ef8acec12c
jdewaen <jorik.de.waen@gmail.com>
2015-03-23 16:47:54 +0100

    Resource boilerplate

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/resource/Reservation.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/resource/Reservation.java
@@ -1,6 +1,49 @@
 package be.kuleuven.cs.swop.domain.resource;
 
+import be.kuleuven.cs.swop.domain.TimePeriod;
+import be.kuleuven.cs.swop.domain.task.Task;
+
 
 public class Reservation {
+    private Resource resource;
+    private TimePeriod reservedDuring;
+    private Task owner;
 
+
+    public Reservation(Resource resource, TimePeriod reservedDuring,Task owner){
+        this.setResource(resource);
+        this.setReservedDuring(reservedDuring);
+        this.setOwner(owner);
+    }
+    protected boolean canHaveAsResource(Resource resource){
+        return resource != null;
+    }
+
+
+    protected boolean canHaveAsTimePerioid(TimePeriod period){
+        return period != null;
+    }
+
+
+    protected boolean canHaveAsOwner(Task owner){
+        return owner != null;
+    }
+    public Resource getResource() {
+        return resource;
+    }
+    private void setResource(Resource resource) {
+        this.resource = resource;
+    }
+    public TimePeriod getReservedDuring() {
+        return reservedDuring;
+    }
+    private void setReservedDuring(TimePeriod reservedDuring) {
+        this.reservedDuring = reservedDuring;
+    }
+    public Task getOwner() {
+        return owner;
+    }
+    private void setOwner(Task owner) {
+        this.owner = owner;
+    }
 }
