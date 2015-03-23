e1bf42b6ab9b4904d6574b53e22d00be86f3602b
Michiel Derhaeg <derhaeg_michiel@hotmail.com>
2015-03-23 18:32:07 +0100

    added boilerplate for resourceType #12

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/resource/ResourceType.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/resource/ResourceType.java
@@ -1,6 +1,62 @@
 package be.kuleuven.cs.swop.domain.resource;
 
+import java.util.Set;
+import java.util.HashSet;
+
+import com.google.common.collect.ImmutableSet;
 
 public class ResourceType {
 
+    private String name;
+    private Set<ResourceType> requirements;
+    private Set<ResourceType> conflictsWith;
+
+    public ResourceType(String name, Set<ResourceType> requirements, Set<ResourceType> conflicts) {
+        this.setName(name);
+        this.setRequirements(requirements);
+        this.setConflictsWith(conflicts);
+    }
+
+    public Set<ResourceType> getRequirements() {
+        return ImmutableSet.copyOf(this.requirements);
+    }
+
+    private void setRequirements(Set<ResourceType> requirements) {
+        if(!canHaveAsRequirements(requirements)) throw new IllegalArgumentException(ERROR_ILLEGAL_REQUIREMENTS);
+        this.requirements = requirements;
+    }
+
+    protected boolean canHaveAsRequirements(Set<ResourceType> requirements) {
+        return requirements != null;
+    }
+
+    public Set<ResourceType> getConflictsWith() {
+        return ImmutableSet.copyOf(this.conflictsWith);
+    }
+
+    private void setConflictsWith(Set<ResourceType> conflictsWith) {
+        if(!canHaveAsConflictsWith(conflictsWith)) throw new IllegalArgumentException(ERROR_ILLEGAL_CONFLICTS);
+        this.conflictsWith = conflictsWith;
+    }
+
+    protected boolean canHaveAsConflictsWith(Set<ResourceType> conflictsWith) {
+        return conflictsWith != null;
+    }
+
+    private void setName(String name) {
+        if(!canHaveAsName(name)) throw new IllegalArgumentException(ERROR_ILLEGAL_NAME);
+        this.name = name;
+    }
+
+    protected boolean canHaveAsName(String name) {
+        return name != null && !name.isEmpty();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private static final String ERROR_ILLEGAL_REQUIREMENTS    = "Illegal requirement set for resource type.";
+    private static final String ERROR_ILLEGAL_CONFLICTS    = "Illegal conflict set for resource type.";
+    private static final String ERROR_ILLEGAL_NAME    = "Illegal name for resource type.";
 }
