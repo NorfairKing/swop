99cb2a0ec90521dbdf75fb2d1c84529b9b8c7db4
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-03-23 16:48:16 +0100

    requirement boiler plate  fixed #1

---
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/resource/Requirement.java
@@ -0,0 +1,43 @@
+package be.kuleuven.cs.swop.domain.resource;
+
+
+public class Requirement {
+
+    private int          amount;
+    private ResourceType type;
+    
+    public Requirement(int amount, ResourceType type){
+        setAmount(amount);
+        setType(type);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    protected boolean canHaveAsAmount(int amount) {
+        return amount > 0;
+    }
+
+    public void setAmount(int amount) {
+        if (!canHaveAsAmount(amount)) { throw new IllegalArgumentException(ERROR_ILLEGAL_AMOUNT); }
+        this.amount = amount;
+    }
+
+    public ResourceType getType() {
+        return type;
+    }
+
+    protected boolean canHaveAsType(ResourceType type) {
+        return type != null;
+    }
+
+    public void setType(ResourceType type) {
+        if (!canHaveAsType(type)) { throw new IllegalArgumentException(ERROR_ILLEGAL_TYPE); }
+        this.type = type;
+    }
+
+    private static String ERROR_ILLEGAL_AMOUNT = "Illegal amount for requirement";
+    private static String ERROR_ILLEGAL_TYPE   = "Illegal type for requirement.";
+
+}
