90c92aa73bca11c3d691532b64d16aeadc43a471
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-03-23 16:13:29 +0100

    fixed #11

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/resource/Resource.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/resource/Resource.java
@@ -2,24 +2,25 @@ package be.kuleuven.cs.swop.domain.resource;
 
 
 public class Resource {
+
     private ResourceType type;
-    
-    public Resource(ResourceType type){
+
+    public Resource(ResourceType type) {
         setType(type);
     }
 
     public ResourceType getType() {
         return type;
     }
-    
-    protected boolean canHaveAsType(ResourceType type){
+
+    protected boolean canHaveAsType(ResourceType type) {
         return type != null;
     }
 
     private void setType(ResourceType type) {
-        if (!canHaveAsType(type)){throw new IllegalArgumentException(ERROR_ILLEGAL_TYPE);}
+        if (!canHaveAsType(type)) { throw new IllegalArgumentException(ERROR_ILLEGAL_TYPE); }
         this.type = type;
     }
-    
+
     private static final String ERROR_ILLEGAL_TYPE = "Illegal resource type for resource.";
 }
