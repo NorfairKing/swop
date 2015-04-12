a7aa5ce15d5b477a1464d6d5229d9eec9a78817f
jdewaen <jorik.de.waen@gmail.com>
2015-04-12 17:33:13 +0200

    forgot to add file

--- swapp/src/main/java/be/kuleuven/cs/swop/YAMLReader.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/YAMLReader.java
@@ -37,250 +37,294 @@ import be.kuleuven.cs.swop.facade.TaskWrapper;
 public class YAMLReader {
 
 	TaskMan facade;
+	
+	DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+	DateTimeFormatter timeformat = DateTimeFormatter.ofPattern("HH:mm");
+	
+
+	// Remember variables in order
+	List<ProjectWrapper> projects = new ArrayList<ProjectWrapper>();
+	List<TaskWrapper> tasks = new ArrayList<TaskWrapper>();
+	List<ResourceTypeWrapper> resourceTypes = new ArrayList<ResourceTypeWrapper>();
+	List<ResourceWrapper> resources = new ArrayList<ResourceWrapper>();
+	List<DeveloperWrapper> developers = new ArrayList<DeveloperWrapper>();
+	List<LocalTime[]> availabilities = new ArrayList<LocalTime[]>();
+	Map<TaskWrapper, Integer> planningTasks = new HashMap<TaskWrapper, Integer>();
+	Map<Integer,Set<DeveloperWrapper>> planningDevelopers = new HashMap<Integer,Set<DeveloperWrapper>>();
+	Map<Integer,LocalDateTime> planningStartDates = new HashMap<Integer,LocalDateTime>();
+	Map<TaskWrapper, Set<ResourceWrapper>> taskResources = new HashMap<TaskWrapper, Set<ResourceWrapper>>();
 
 	public YAMLReader(TaskMan facade) {
 		this.facade = facade;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Map<String, List<Map<String, Object>>> readFile(String initFile) throws FileNotFoundException{
+		File f = new File(initFile);
+		InputStream input = new FileInputStream(f);
+		Yaml yaml = new Yaml();
+		return (Map<String, List<Map<String, Object>>>) yaml.load(input);
+	}
+	
+	
+	private void processSystemTime(Map<String, List<Map<String, Object>>> parsedFile){
+		Object dateObject = parsedFile.get("systemTime");
+		String dateString = dateObject.toString();
+		facade.updateSystemTime(LocalDateTime.parse(dateString, format));
+	}
+	
+	
+	private void processDailyAvailabilities(Map<String, List<Map<String, Object>>> parsedFile){
+		for (Map<String, Object> avail : parsedFile.get("dailyAvailability")) {
+			if (avail != null) {
+				Object start = avail.get("startTime");
+				Object end = avail.get("endTime");
+				if(start != null && end != null){
+					LocalTime[] period = new LocalTime[2];
+					period[0] = LocalTime.parse(start.toString(),timeformat);
+					period[1] = LocalTime.parse(end.toString(),timeformat);
+					availabilities.add(period);
+				}
+			}
+		}
+	}
+	
+	private void processResourceTypes(Map<String, List<Map<String, Object>>> parsedFile){
+		for (Map<String, Object> resourceType : parsedFile.get("resourceTypes")) {
+
+			// Requirements
+			Set<ResourceTypeWrapper> requirements = new HashSet<ResourceTypeWrapper>();
+			List<Integer> reqs = (List<Integer>) resourceType.get("requires");
+			if (reqs != null) {
+				for (int index : reqs) {
+					ResourceTypeWrapper req = resourceTypes.get(index);
+					requirements.add(req);                        
+				}
+			}
+
+			// Conflicts
+			Set<ResourceTypeWrapper> conflicts = new HashSet<ResourceTypeWrapper>();
+			List<Integer> confs = (List<Integer>) resourceType.get("conflictsWith");
+			boolean selfConflicting = false; // Resourcetypes can conflict with themselves.
+			if (confs != null) {
+				for (int index : confs) {
+					if(index == resourceTypes.size()){
+						selfConflicting = true;
+						continue;
+					}
+					ResourceTypeWrapper conf = resourceTypes.get(index);
+					conflicts.add(conf);                                                
+				}
+			}
+			
+			Object dailyAvail = resourceType.get("dailyAvailability");
+			LocalTime[] avail = new LocalTime[0];
+			if(dailyAvail != null){
+				avail = availabilities.get((int) dailyAvail);
+			}
+			
+			String name = (String) resourceType.get("name");
+			ResourceTypeWrapper newType = facade.createResourceType(new ResourceTypeData(name, requirements, conflicts, selfConflicting, avail));
+			
+			resourceTypes.add(newType);
+		}
+	}
+	
+	private void processResources(Map<String, List<Map<String, Object>>> parsedFile){
+		for (Map<String, Object> resource : parsedFile.get("resources")) {
+			String name = (String) resource.get("name");
+			int typeId = (int) resource.get("type");
+			ResourceTypeWrapper type = resourceTypes.get(typeId);
+			ResourceWrapper newRes = facade.createResource(new ResourceData(name, type));
+			resources.add(newRes);
+		}
+	}
+	
+	private void processDevelopers(Map<String, List<Map<String, Object>>> parsedFile){
+		for (Map<String, Object> developer : parsedFile.get("developers")) {
+			String name = (String) developer.get("name");
+			DeveloperWrapper dev = facade.createDeveloper(new DeveloperData(name));
+			developers.add(dev);
+		}
+	}
+	
+	private void processProjects(Map<String, List<Map<String, Object>>> parsedFile){
+		for (Map<String, Object> project : parsedFile.get("projects")) {
+			ProjectData pData = new ProjectData(
+					(String) project.get("name"),
+					(String) project.get("description"),
+					LocalDateTime.parse((String) project.get("creationTime"), format),
+					LocalDateTime.parse((String) project.get("dueTime"), format)
+					);
+			ProjectWrapper p = facade.createProject(pData);
+			projects.add(p);
+		}
+	}
+	
+	private void processTasks(Map<String, List<Map<String, Object>>> parsedFile){
+		for (Map<String, Object> task : parsedFile.get("tasks")) {
+			// Get project of this task
+			int projectIndex = (int) task.get("project");
+			ProjectWrapper project = projects.get(projectIndex);
+			// Create task
+			TaskData tData = new TaskData(
+					(String) task.get("description"),
+					(long) (int) task.get("estimatedDuration"),
+					(double) (int) task.get("acceptableDeviation")/100
+					);
+			TaskWrapper t = facade.createTaskFor(project, tData);
+
+			// Add dependencies
+			List<Integer> prerequisites = (List<Integer>) task.get("prerequisiteTasks");
+			if (prerequisites != null) {
+				for (int index : prerequisites) {
+					TaskWrapper prereq = tasks.get(index);
+					facade.addDependencyTo(t, prereq);
+				}
+			}
+
+			// Add as alternative to other(s)
+			Object alter = task.get("alternativeFor");
+			if (alter != null) {
+				TaskWrapper other = tasks.get((int) alter);
+				facade.setAlternativeFor(other, t);
+			}
+
+			// Finish/fail task if set
+			String status = (String) task.get("status");
+			if (status != null) {
+				boolean successful = false;
+				boolean isFinal = false;
+				boolean executing = false;
+				switch (status) {
+				case "finished":
+					successful = true;
+					isFinal = true;
+					break;
+				case "failed":
+					successful = false;
+					isFinal = true;
+					break;
+				case "executing":
+					executing = true;
+					break;
+				default:
+					throw new IllegalStateException("Unknown status.");
+				}
+				TaskStatusData statusData = null;
+				if(isFinal){
+					// Need to start executing the task before we can finish it
+					facade.updateTaskStatusFor(t, new ExecutingStatusData());   
+					LocalDateTime startTime = LocalDateTime.parse((String) task.get("startTime"), format);
+					LocalDateTime endTime = LocalDateTime.parse((String) task.get("endTime"), format);
+					
+					if(successful){
+						statusData = new FinishedStatusData(startTime, endTime);
+					}else{
+						statusData = new FailedStatusData(startTime, endTime);
+					}
+				}else{
+					if(executing){
+						statusData = new ExecutingStatusData();
+					}
+				}
+				facade.updateTaskStatusFor(t, statusData);   
+
+			}
+
+			// Keep track for planning
+			Object planning = task.get("planning");
+			if(planning != null){
+				planningTasks.put(t, (Integer) planning);
+			}
+
+			taskResources.put(t, new HashSet<ResourceWrapper>());
+
+
+			// Add to tracking list
+			tasks.add(t);
+
+		}
+	}
+	
+	private void preProcessPlannings(Map<String, List<Map<String, Object>>> parsedFile){
+		int planningIndex = 0;
+		for (Map<String, Object> planning : parsedFile.get("plannings")) {
+			LocalDateTime startTime = LocalDateTime.parse((String) planning.get("plannedStartTime"), format);
+			planningStartDates.put(planningIndex, startTime);
+			// Add developers
+			List<Integer> devs = (List<Integer>) planning.get("developers");
+			Set<DeveloperWrapper> currentDevs = new HashSet<DeveloperWrapper>();
+			if (devs != null) {
+				for (int index : devs) {
+					DeveloperWrapper dev = developers.get(index);
+					currentDevs.add(dev);
+				}
+			}
+			planningDevelopers.put(planningIndex, currentDevs);
+			planningIndex++;
+		}
+	}
+	
+	private void processReservations(Map<String, List<Map<String, Object>>> parsedFile){
+		for (Map<String, Object> reservation : parsedFile.get("reservations")) {
+			int resourceId = (Integer) reservation.get("resource");
+			int taskId = (Integer) reservation.get("task");
+			TaskWrapper task = tasks.get(taskId);
+			ResourceWrapper resource = resources.get(resourceId);
+			taskResources.get(task).add(resource);
+		}
+	}
+	
+	private void processPlannings(Map<String, List<Map<String, Object>>> parsedFile){
+		for(TaskWrapper task : tasks){
+			if(planningTasks.containsKey(task)){
+				int planningId = planningTasks.get(task);
+				Set<DeveloperWrapper> devs = planningDevelopers.get(planningId);
+				LocalDateTime startTime = planningStartDates.get(planningId);
+				Set<ResourceWrapper> resourceSet = taskResources.get(task);
+				facade.createPlanning(task, startTime, resourceSet, devs);
+			}
+		}
 	}
 
 	@SuppressWarnings("unchecked")
 	public void read(String initFile) {
 		try {
 			// Setup
-			File f = new File(initFile);
-			InputStream input = new FileInputStream(f);
-			Yaml yaml = new Yaml();
-			DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-			DateTimeFormatter timeformat = DateTimeFormatter.ofPattern("HH:mm");
-			Map<String, List<Map<String, Object>>> parsedFile = (Map<String, List<Map<String, Object>>>) yaml.load(input);
-
-			// Remember variables in order
-			List<ProjectWrapper> projects = new ArrayList<ProjectWrapper>();
-			List<TaskWrapper> tasks = new ArrayList<TaskWrapper>();
-			List<ResourceTypeWrapper> resourceTypes = new ArrayList<ResourceTypeWrapper>();
-			List<ResourceWrapper> resources = new ArrayList<ResourceWrapper>();
-			List<DeveloperWrapper> developers = new ArrayList<DeveloperWrapper>();
-			List<LocalTime[]> availabilities = new ArrayList<LocalTime[]>();
-			Map<TaskWrapper, Integer> planningTasks = new HashMap<TaskWrapper, Integer>();
-			Map<Integer,Set<DeveloperWrapper>> planningDevelopers = new HashMap<Integer,Set<DeveloperWrapper>>();
-			Map<Integer,LocalDateTime> planningStartDates = new HashMap<Integer,LocalDateTime>();
-			Map<TaskWrapper, Set<ResourceWrapper>> taskResources = new HashMap<TaskWrapper, Set<ResourceWrapper>>();
+			Map<String, List<Map<String, Object>>> parsedFile = readFile(initFile);
 
 			// Set system time
-			Object dateObject = parsedFile.get("systemTime");
-			String dateString = dateObject.toString();
-			facade.updateSystemTime(LocalDateTime.parse(dateString, format));
+			processSystemTime(parsedFile);
 			
 			// Read dailyAvailabities
-			for (Map<String, Object> avail : parsedFile.get("dailyAvailability")) {
-				if (avail != null) {
-					Object start = avail.get("startTime");
-					Object end = avail.get("endTime");
-					if(start != null && end != null){
-						LocalTime[] period = new LocalTime[2];
-						period[0] = LocalTime.parse(start.toString(),timeformat);
-						period[1] = LocalTime.parse(end.toString(),timeformat);
-						availabilities.add(period);
-					}
-				}
-			}
+			processDailyAvailabilities(parsedFile);
 
 			// Read resourceTypes
-			for (Map<String, Object> resourceType : parsedFile.get("resourceTypes")) {
-
-				// Requirements
-				Set<ResourceTypeWrapper> requirements = new HashSet<ResourceTypeWrapper>();
-				List<Integer> reqs = (List<Integer>) resourceType.get("requires");
-				if (reqs != null) {
-					for (int index : reqs) {
-						ResourceTypeWrapper req = resourceTypes.get(index);
-						requirements.add(req);                        
-					}
-				}
-
-				// Conflicts
-				Set<ResourceTypeWrapper> conflicts = new HashSet<ResourceTypeWrapper>();
-				List<Integer> confs = (List<Integer>) resourceType.get("conflictsWith");
-				boolean selfConflicting = false; // Resourcetypes can conflict with themselves.
-				if (confs != null) {
-					for (int index : confs) {
-						if(index == resourceTypes.size()){
-							selfConflicting = true;
-							continue;
-						}
-						ResourceTypeWrapper conf = resourceTypes.get(index);
-						conflicts.add(conf);                                                
-					}
-				}
-				
-				Object dailyAvail = resourceType.get("dailyAvailability");
-				LocalTime[] avail = new LocalTime[0];
-				if(dailyAvail != null){
-					avail = availabilities.get((int) dailyAvail);
-				}
-				
-				String name = (String) resourceType.get("name");
-				ResourceTypeWrapper newType = facade.createResourceType(new ResourceTypeData(name, requirements, conflicts, selfConflicting, avail));
-				
-				resourceTypes.add(newType);
-			}
+			processResourceTypes(parsedFile);
 
 			// Add resources
-			for (Map<String, Object> resource : parsedFile.get("resources")) {
-				String name = (String) resource.get("name");
-				int typeId = (int) resource.get("type");
-				ResourceTypeWrapper type = resourceTypes.get(typeId);
-				ResourceWrapper newRes = facade.createResource(new ResourceData(name, type));
-				resources.add(newRes);
-			}
+			processResources(parsedFile);
 
 			// Add developers
-			for (Map<String, Object> developer : parsedFile.get("developers")) {
-				String name = (String) developer.get("name");
-				DeveloperWrapper dev = facade.createDeveloper(new DeveloperData(name));
-				developers.add(dev);
-			}
-
-
+			processDevelopers(parsedFile);
 
 			// Read projects
-			for (Map<String, Object> project : parsedFile.get("projects")) {
-				ProjectData pData = new ProjectData(
-						(String) project.get("name"),
-						(String) project.get("description"),
-						LocalDateTime.parse((String) project.get("creationTime"), format),
-						LocalDateTime.parse((String) project.get("dueTime"), format)
-						);
-				ProjectWrapper p = facade.createProject(pData);
-				projects.add(p);
-			}
+			processProjects(parsedFile);
 
 			// Read tasks
-			for (Map<String, Object> task : parsedFile.get("tasks")) {
-				// Get project of this task
-				int projectIndex = (int) task.get("project");
-				ProjectWrapper project = projects.get(projectIndex);
-				// Create task
-				TaskData tData = new TaskData(
-						(String) task.get("description"),
-						(long) (int) task.get("estimatedDuration"),
-						(double) (int) task.get("acceptableDeviation")/100
-						);
-				TaskWrapper t = facade.createTaskFor(project, tData);
-
-				// Add dependencies
-				List<Integer> prerequisites = (List<Integer>) task.get("prerequisiteTasks");
-				if (prerequisites != null) {
-					for (int index : prerequisites) {
-						TaskWrapper prereq = tasks.get(index);
-						facade.addDependencyTo(t, prereq);
-					}
-				}
-
-				// Add as alternative to other(s)
-				Object alter = task.get("alternativeFor");
-				if (alter != null) {
-					TaskWrapper other = tasks.get((int) alter);
-					facade.setAlternativeFor(other, t);
-				}
-
-				// Finish/fail task if set
-				String status = (String) task.get("status");
-				if (status != null) {
-					boolean successful = false;
-					boolean isFinal = false;
-					boolean executing = false;
-					switch (status) {
-					case "finished":
-						successful = true;
-						isFinal = true;
-						break;
-					case "failed":
-						successful = false;
-						isFinal = true;
-						break;
-					case "executing":
-						executing = true;
-						break;
-					default:
-						throw new IllegalStateException("Unknown status.");
-					}
-					TaskStatusData statusData = null;
-					if(isFinal){
-						// Need to start executing the task before we can finish it
-						facade.updateTaskStatusFor(t, new ExecutingStatusData());   
-						LocalDateTime startTime = LocalDateTime.parse((String) task.get("startTime"), format);
-						LocalDateTime endTime = LocalDateTime.parse((String) task.get("endTime"), format);
-						
-						if(successful){
-							statusData = new FinishedStatusData(startTime, endTime);
-						}else{
-							statusData = new FailedStatusData(startTime, endTime);
-						}
-					}else{
-						if(executing){
-							statusData = new ExecutingStatusData();
-						}
-					}
-					facade.updateTaskStatusFor(t, statusData);   
-
-				}
-
-				// Keep track for planning
-				Object planning = task.get("planning");
-				if(planning != null){
-					planningTasks.put(t, (Integer) planning);
-				}
-
-				taskResources.put(t, new HashSet<ResourceWrapper>());
-
-
-				// Add to tracking list
-				tasks.add(t);
-
-			}
+			processTasks(parsedFile);
 
 			// Add developers to plannings
-			int planningIndex = 0;
-			for (Map<String, Object> planning : parsedFile.get("plannings")) {
-				LocalDateTime startTime = LocalDateTime.parse((String) planning.get("plannedStartTime"), format);
-				planningStartDates.put(planningIndex, startTime);
-				// Add developers
-				List<Integer> devs = (List<Integer>) planning.get("developers");
-				Set<DeveloperWrapper> currentDevs = new HashSet<DeveloperWrapper>();
-				if (devs != null) {
-					for (int index : devs) {
-						DeveloperWrapper dev = developers.get(index);
-						currentDevs.add(dev);
-					}
-				}
-				planningDevelopers.put(planningIndex, currentDevs);
-				planningIndex++;
-			}
-
-
+			preProcessPlannings(parsedFile);
 
 			// Add reservations
-			for (Map<String, Object> reservation : parsedFile.get("reservations")) {
-				int resourceId = (Integer) reservation.get("resource");
-				int taskId = (Integer) reservation.get("task");
-				TaskWrapper task = tasks.get(taskId);
-				ResourceWrapper resource = resources.get(resourceId);
-				taskResources.get(task).add(resource);
-			}
+			processReservations(parsedFile);
 
 			// Save all the plannings
-			for(TaskWrapper task : tasks){
-				if(planningTasks.containsKey(task)){
-					int planningId = planningTasks.get(task);
-					Set<DeveloperWrapper> devs = planningDevelopers.get(planningId);
-					LocalDateTime startTime = planningStartDates.get(planningId);
-					Set<ResourceWrapper> resourceSet = taskResources.get(task);
-					facade.createPlanning(task, startTime, resourceSet, devs);
-				}
-			}
+			processPlannings(parsedFile);
 
-
-			System.out.println("Imported.");
+			System.out.println("Successfully imported file.");
 
 		} catch (FileNotFoundException e) {
 			System.out.println("Couldn't import data from file: Not found.");
@@ -289,6 +333,9 @@ public class YAMLReader {
 			e.printStackTrace();
 		} catch (NullPointerException e) {
 			System.out.println("Couldn't import data from file: Missing fields. This probably means you're importing an old file.");
+		} catch (Exception e) {
+			System.out.println("Couldn't import data from file: An error occured.");
+			e.printStackTrace();
 		}
 	}
 }
