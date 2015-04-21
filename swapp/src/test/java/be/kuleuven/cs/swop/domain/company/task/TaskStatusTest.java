3faf827dacf54452fe14a9fd688f564cc95a7f2d
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-04-21 15:33:46 +0200

    Wrote tests for changing status via the company/planning manager #45

--- swapp/src/test/java/be/kuleuven/cs/swop/domain/company/task/TaskStatusTest.java
+++ swapp/src/test/java/be/kuleuven/cs/swop/domain/company/task/TaskStatusTest.java
@@ -2,6 +2,10 @@ package be.kuleuven.cs.swop.domain.company.task;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,16 +14,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import be.kuleuven.cs.swop.domain.company.task.FailedStatus;
-import be.kuleuven.cs.swop.domain.company.task.FinishedStatus;
-import be.kuleuven.cs.swop.domain.company.task.OngoingStatus;
+import be.kuleuven.cs.swop.domain.DateTimePeriod;
+import be.kuleuven.cs.swop.domain.company.Company;
+import be.kuleuven.cs.swop.domain.company.project.Project;
 import be.kuleuven.cs.swop.domain.company.task.Task;
+import be.kuleuven.cs.swop.domain.company.user.Developer;
 
 public class TaskStatusTest {
 
-    private Task             task;
-
-	@Rule
+    Company company;
+    Developer developer;
+    Project project;
+    Task task;
+    
+    @Rule
 	public ExpectedException exception = ExpectedException.none();
 
 	@BeforeClass
@@ -32,12 +40,79 @@ public class TaskStatusTest {
 
 	@Before
 	public void setUp() throws Exception {
+	    company = new Company();
+	    developer = company.createDeveloper("Andy");
+        project = company.createProject("title", "description", LocalDateTime.of(2015, 1, 1, 8, 0), LocalDateTime.of(2015, 1, 1, 16, 0));
+        task = company.createTaskFor(project, "t description", 100, 0.5, new HashSet<>(), new HashSet<>());
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 	
+	@Test
+	public void testViaPlanMan() {
+	    // can't start executing yet, because it isn't planned yet
+	    try {
+	        company.startExecutingTask(task, LocalDateTime.of(2015, 1, 1, 8, 0), developer);
+	        fail();
+	    }
+	    catch (IllegalStateException e) { }
+	    
+	    company.createPlanning(task, LocalDateTime.of(2015, 1, 1, 8, 0),
+	            new HashSet<>(),
+	            new HashSet<>(Arrays.asList(developer)));
+	    
+	    company.startExecutingTask(task, LocalDateTime.of(2015, 1, 1, 8, 0), developer);
+        
+	    assertTrue(task.isExecuting());
+	}
+	
+	@Test
+    public void testViaPlanManFinish() {
+        company.createPlanning(task, LocalDateTime.of(2015, 1, 1, 8, 0),
+                new HashSet<>(),
+                new HashSet<>(Arrays.asList(developer)));
+        
+        company.startExecutingTask(task, LocalDateTime.of(2015, 1, 1, 8, 0), developer);
+        
+        assertTrue(task.isExecuting());
+        
+        company.finishTask(task, new DateTimePeriod(LocalDateTime.of(2015, 1, 1, 8, 0), LocalDateTime.of(2015, 1, 1, 9, 0)));
+        
+        assertTrue(task.isFinished());
+    }
+	
+	@Test
+    public void testViaPlanManFail() {
+        company.createPlanning(task, LocalDateTime.of(2015, 1, 1, 8, 0),
+                new HashSet<>(),
+                new HashSet<>(Arrays.asList(developer)));
+        
+        company.startExecutingTask(task, LocalDateTime.of(2015, 1, 1, 8, 0), developer);
+        
+        assertTrue(task.isExecuting());
+        
+        company.failTask(task, new DateTimePeriod(LocalDateTime.of(2015, 1, 1, 8, 0), LocalDateTime.of(2015, 1, 1, 9, 0)));
+        
+        assertTrue(task.isFailed());
+    }
+	
 
+    
+    @Test
+    public void testViaPlanManOngoing() {
+        assertFalse(task.isExecuting());
+        assertFalse(task.isFailed());
+        assertFalse(task.isFinished());
+        
+        company.createPlanning(task, LocalDateTime.of(2015, 1, 1, 8, 0),
+                new HashSet<>(),
+                new HashSet<>(Arrays.asList(developer)));
+        
+        assertFalse(task.isExecuting());
+        assertFalse(task.isFailed());
+        assertFalse(task.isFinished());
+    }
 
 }
