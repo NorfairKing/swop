9aa2704ce30ebea2ef835b31093d2b9fd9201a58
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-05-21 20:49:30 +0200

    fixed another test

--- swapp/src/test/java/be/kuleuven/cs/swop/domain/company/planning/TaskPlanningTest.java
+++ swapp/src/test/java/be/kuleuven/cs/swop/domain/company/planning/TaskPlanningTest.java
@@ -1,5 +1,6 @@
 package be.kuleuven.cs.swop.domain.company.planning;
 
+
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,15 +11,16 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import be.kuleuven.cs.swop.domain.DateTimePeriod;
 import be.kuleuven.cs.swop.domain.company.resource.Resource;
 import be.kuleuven.cs.swop.domain.company.task.Task;
 import be.kuleuven.cs.swop.domain.company.user.Developer;
 
 
 public class TaskPlanningTest {
-    
+
     Set<Resource> resSet;
-    Task task;
+    Task          task;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {}
@@ -28,7 +30,7 @@ public class TaskPlanningTest {
 
     @Before
     public void setUp() throws Exception {
-        
+
         resSet = new HashSet<>();
         resSet.add(new Developer("Sean"));
     }
@@ -38,18 +40,18 @@ public class TaskPlanningTest {
 
     @Test
     public void constructorValidTest() {
-        new TaskPlanning(LocalDateTime.of(2015, 02, 20, 12, 0), resSet, 50);
+        new TaskPlanning(new DateTimePeriod(LocalDateTime.of(2015, 02, 20, 12, 0),LocalDateTime.of(2015, 02,20,13,0)), resSet);
     }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void constructorInvalidResourcesTest(){
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorInvalidResourcesTest() {
         resSet.add(null);
-        new TaskPlanning(LocalDateTime.of(2015, 02, 20, 12, 0), resSet, 50);
+        new TaskPlanning(new DateTimePeriod(LocalDateTime.of(2015, 02, 20, 12, 0),LocalDateTime.of(2015, 02,20,13,0)), resSet);
     }
-    
-    @Test(expected=IllegalArgumentException.class)
-    public void constructorInvalidPlannedStartTimeTest(){
-        new TaskPlanning(null, resSet, 50);
+
+    @Test(expected = IllegalArgumentException.class)
+    public void constructorInvalidPlannedStartTimeTest() {
+        new TaskPlanning(null, resSet);
     }
 
 }
