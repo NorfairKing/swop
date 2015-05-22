4ff1e6d3ff62ae31baf45b4f9ac9b2bbf3f266fc
Michiel Derhaeg <derhaeg_michiel@hotmail.com>
2015-05-22 15:07:49 +0200

    docs for BranchOffice

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/company/BranchOffice.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/company/BranchOffice.java
@@ -49,11 +49,20 @@ public class BranchOffice implements Serializable {
     }
 
     // Getters and setters of internal state
+    /**
+     * Restrieves the location of this office.
+     *
+     * @return The String with the location of this BranchOffice.
+     */
     public String getLocation() {
         return location;
     }
 
-    // Passthrough getters
+    /**
+     * Retrieves all developers that work at this office.
+     *
+     * @return a Set containing all Developers of this BranchOffice.
+     */
     public Set<Developer> getDevelopers() {
         Set<Developer> devs = new HashSet<Developer>();
         for(Resource res : resources){
@@ -64,22 +73,47 @@ public class BranchOffice implements Serializable {
         return devs;
     }
 
+    /**
+     * Retrieves all projects this office does.
+     *
+     * @return An ImmutableSet containing all of the Projects.
+     */
     public ImmutableSet<Project> getProjects() {
         return ImmutableSet.copyOf(projects);
     }
 
+    /**
+     * Retrieves all resources this office has.
+     *
+     * @return A Set containing all of the Resources.
+     */
     public Set<Resource> getResources() {
         return new HashSet<Resource>(resources);
     }
 
+    /**
+     * Retrieves all the types of resources this office has.
+     *
+     * @return A Set containing all of the ResourceTypes.
+     */
     public Set<ResourceType> getResourceTypes() {
         return company.getResourceTypes();
     }
-    
+
+    /**
+     * Retrieves a Project containing all of that Tasks that are delicated to this office.
+     *
+     * @return The delegation Project.
+     */
     public Project getDelegationProject(){
         return this.delegationProject;
     }
 
+    /**
+     * Retrieves every planning of every task of this office.
+     *
+     * @return A Set with all of the TaskPlannings.
+     */
     public Set<TaskPlanning> getTaskPlannings() {
         Set<TaskPlanning> plannings = new HashSet<TaskPlanning>();
         for (Project proj : getProjects()) {
@@ -98,7 +132,7 @@ public class BranchOffice implements Serializable {
 
     /**
      * Tries to find the project of the given task and returns it. Returns null if no project was found, or the given task was null.
-     * 
+     *
      * @param task
      *            The task to find the project for
      * @return The project to which the task belongs
@@ -111,6 +145,12 @@ public class BranchOffice implements Serializable {
         return null;
     }
 
+    /**
+     * Retrieves every task that still needs to be planned of the give project.
+     *
+     * @param project The Project that will be checked for unplanned Tasks.
+     * @return A Set containing all of the unplanned Tasks.
+     */
     public Set<Task> getUnplannedTasksOf(Project project) {
         return this.planningDepartment.getUnplannedTasksFrom(project.getTasks());
     }
@@ -126,10 +166,24 @@ public class BranchOffice implements Serializable {
         return this.planningDepartment.getPlanningsFor(project.getTasks());
     }
 
+    /**
+     * Retrieve the tasks of the given project assigned to the given developer.
+     *
+     * @param project The Project that contains the Tasks.
+     * @param dev The Developer that is assigned to the retrieved Tasks.
+     * @return A Set containing all of the assigned Tasks.
+     */
     public Set<Task> getAssignedTasksOf(Project project, Developer dev) {
         return this.planningDepartment.getAssignedTasksOf(project.getTasks(), dev);
     }
 
+    /**
+     * Retrieves the task of a given planning.
+     *
+     * @param planning The TaskPlanning for which the retrieved Task is meant for.
+     *
+     * @return The Task for wich the given TaskPlanning was meant for.
+     */
     public Task getTaskFor(TaskPlanning planning) {
         for (Project proj : getProjects()) {
             for (Task task : proj.getTasks()) {
@@ -139,22 +193,67 @@ public class BranchOffice implements Serializable {
         return null;
     }
 
+    /**
+     * Retrieves several options for when the given Task can be planned.
+     *
+     * @param task The Task for which the time options will be retrieved.
+     * @param amount How many options will be retrieved.
+     * @param time The retrieved times will be after this LocalDateTime.
+     * @return A List containing the LocalDateTimes.
+     */
     public List<LocalDateTime> getPlanningTimeOptions(Task task, int amount, LocalDateTime time) {
         return this.planningDepartment.getPlanningTimeOptions(task, amount, time);
     }
 
+    /**
+     * Retrieves the Resources which can be reserved for planning the given Task
+     * at the given time.
+     *
+     * @param task The to be planned Task.
+     * @param time The LocalDateTime when the Task would be planned.
+     * @return A Map containing the ResourcesTypes the given Task needs
+     * and Lists containig the available Resources of that type.
+     */
     public Map<ResourceType, List<Resource>> getPlanningResourceOptions(Task task, LocalDateTime time) {
         return this.planningDepartment.getPlanningResourceOptions(task, time);
     }
 
+    /**
+     * Retrieves the Developers which can be assigned to the Task for the given
+     * time.
+     *
+     * @param task The to be planned Task.
+     * @param time The LocalDateTime when the Task would be planned.
+     * @return A Set containing the Developers.
+     */
     public Set<Developer> getPlanningDeveloperOptions(Task task, LocalDateTime time) {
         return this.planningDepartment.getPlanningDeveloperOptions(task, time);
     }
 
+    /**
+     * Creates a new planning for the given Task.
+     *
+     * @param task The to be planned Task.
+     * @param time The LocalDateTime when the given Task will be planned.
+     * @param rss A Set containing the reserved Resources.
+     * @param withBreak A boolean that specifies whether or not the planning should
+     * include a break for the developers.
+     * @throws ConflictingPlannedTaskException When the to be created TaskPlanning would
+     * cause conflicts.
+     */
     public void createPlanning(Task task, LocalDateTime time, Set<Resource> rss, boolean withBreak) throws ConflictingPlannedTaskException {
         this.planningDepartment.createPlanning(task, new DateTimePeriod(time, time.plusMinutes(task.getEstimatedDuration())), rss, withBreak);
     }
 
+    /**
+     * Creates a new planning for the given Task.
+     *
+     * @param task The to be planned Task.
+     * @param time The LocalDateTime when the given Task will be planned.
+     * @param rss A Set containing the reserved Resources.
+     * @throws ConflictingPlannedTaskException When the to be created TaskPlanning would
+     * cause conflicts.
+     */
     public void createPlanning(Task task, LocalDateTime time, Set<Resource> rss) throws ConflictingPlannedTaskException {
         createPlanning(task, time, rss, false);
     }
@@ -219,14 +318,36 @@ public class BranchOffice implements Serializable {
     // finish, fail and executing has to happen through the planningManager
     // that's the class that can decide about this
     // We can't enforce this in java, but we enforce it with mind-power
+
+    /**
+     * Set the given task to finished.
+     * @param t The Task that is now finished.
+     * @throws ConflictingPlannedTaskException If this causes a conflict with another
+     * planning.
+     */
     public void finishTask(Task t, LocalDateTime time) throws ConflictingPlannedTaskException {
         this.planningDepartment.finishTask(t, time);
     }
 
+    /**
+     * Set the given task to failed.
+     * @param t The Task that is now failed.
+     * @throws ConflictingPlannedTaskException If this causes a conflict with another
+     * planning.
+     */
     public void failTask(Task t, LocalDateTime time) throws ConflictingPlannedTaskException {
         this.planningDepartment.failTask(t, time);
     }
 
+    /**
+     * Set the given taks to an executing status.
+     *
+     * @param t The Task that will now start executing.
+     * @param resources A Set containing the Resources that the Tasks reserves.
+     * @throws ConflictingPlannedTaskException If this creates a conflict with another
+     * planning.
+     * @throws IllegalArgumentException If a manager tries to execute a Task.
+     */
     public void startExecutingTask(Task t, LocalDateTime time, Set<Resource> resources) throws ConflictingPlannedTaskException {
         this.planningDepartment.startExecutingTask(t, time, resources);
     }
@@ -245,7 +366,7 @@ public class BranchOffice implements Serializable {
         resources.add(resource);
         return resource;
     }
-    
+
     public Set<User> getUsers() {
         return ImmutableSet.copyOf(this.employees);
     }
@@ -263,18 +384,44 @@ public class BranchOffice implements Serializable {
         employees.add(dev);
         return dev;
     }
-    
+
+    /**
+     * Creates a new manager with the given name.
+     *
+     * @return The newly created Manager.
+     */
     public Manager createManager(String name) {
         Manager man = new Manager(name);
         employees.add(man);
         return man;
     }
 
+    /**
+     * Checks whether this office has the given task.
+     *
+     * @param task The Task that will be checked.
+     * @return True if this BranchOffice has this Task.
+     */
     public boolean hasTask(Task task) {
         Project proj = getProjectFor(task);
         return proj != null;
     }
 
+    /**
+     * Creates a new Task that was delegated from another office.
+     *
+     * @param description
+     *            The description for the new Task.
+     *
+     * @param estimatedDuration
+     *            The estimated duration it will take to complete the Task.
+     *
+     * @param acceptableDeviation
+     *            The acceptable deviation of time for completing the Task.
+     *
+     * @return The newly created Task.
+     *
+     */
     public Task createDelegationTask(String description, long estimatedDuration, double acceptableDeviation, Requirements requirements) {
         Task task = delegationProject.createTask(description, estimatedDuration, acceptableDeviation, requirements);
         return task;
@@ -299,15 +446,15 @@ public class BranchOffice implements Serializable {
     public void restoreFromMemento(Memento memento) {
         this.resources.clear();
         this.resources.addAll(memento.resources);
-        
+
         this.projects.clear();
         this.projects.addAll(memento.projects);
-        
+
         this.delegationProject = memento.delegationProject;
     }
 
     public static class Memento implements Serializable {
-    	
+
         private final Set<Resource> resources;
         private final Set<Project> projects;
         private final Project delegationProject;
