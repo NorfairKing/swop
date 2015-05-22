862602711a3e760b18b9ac4acd50de6230ea2884
Michiel Derhaeg <derhaeg_michiel@hotmail.com>
2015-05-22 15:53:29 +0200

    docs for Requirement

--- swapp/src/main/java/be/kuleuven/cs/swop/domain/company/resource/Requirement.java
+++ swapp/src/main/java/be/kuleuven/cs/swop/domain/company/resource/Requirement.java
@@ -16,19 +16,36 @@ public class Requirement implements Serializable {
     public Requirement(int amount, ResourceType type) {
         if (type == null) throw new IllegalArgumentException(ERROR_ILLEGAL_TYPE);
         if (amount <= 0) throw new IllegalArgumentException(ERROR_ILLEGAL_AMOUNT);
-        
+
         this.amount = amount;
         this.type = type;
     }
 
+    /**
+     * Retrieve the amount that is required of the type.
+     *
+     * @return The amount as an integer.
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Retrieve the type of resource that is required.
+     *
+     * @return The ResourceType that is required.
+     */
     public ResourceType getType() {
         return type;
     }
 
+    /**
+     * Checks whether or not the given set of resources satisfies this single requirement.
+     *
+     * @param resources A Set of Resources to be checked for satisfaction for this
+     * Requirement
+     * @return True if this Requirement is satisfied.
+     */
     public boolean isSatisfiedWith(Set<Resource> resources) {
         int counter = 0;
         for (Resource resource : resources) {
